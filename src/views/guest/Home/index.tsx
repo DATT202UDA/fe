c2fe8ea377aa627d07edb7300ccd2a6148012334
NguyenVanNguyen03 <3zuilachinh3@gmail.com>
2025-04-19 16:41:50 +0700

    feature-ui-home

--- src/views/guest/Home/index.tsx
+++ src/views/guest/Home/index.tsx
@@ -1,5 +1,25 @@
+'use client';
+
+import HeroSlider from './components/HeroSlider';
+import Services from './components/Services';
+import Promotions from './components/Promotions';
+import Categories from './components/Categories';
+import NewArrivals from './components/NewArrivals';
+import OnlineExclusive from './components/OnlineExclusive';
+import RecommendedProducts from './components/RecommendedProducts';
+
 const HomeView = () => {
-  return <div>HOME VIEW</div>;
+  return (
+    <div className="min-h-screen">
+      <HeroSlider />
+      <Services />
+      <Promotions />
+      <Categories />
+      <NewArrivals />
+      <OnlineExclusive />
+      <RecommendedProducts />
+    </div>
+  );
 };
 
 export default HomeView;
