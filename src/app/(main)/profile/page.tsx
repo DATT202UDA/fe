bc88679407cac992bd9b61b2f980006a923124e2
Hoang Phuc <hoangphuc91dtt@gmail.com>
2025-04-19 15:36:16 +0700

    create profile

---
+++ src/app/(main)/profile/page.tsx
@@ -0,0 +1,9 @@
+import { ProfileForm } from '@/views/main/Profile/components/Profile/ProfileForm';
+
+export default function ProfilePage() {
+  return (
+    <div className="container mx-auto py-8">
+      <ProfileForm />
+    </div>
+  );
+} 
--- src/views/main/Profile/components/Profile/index.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export const ProfileForm = () => {
-  return <div>This is the profile form</div>;
-};
